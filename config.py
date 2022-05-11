0915c37eefa763b933454aa2e9a357cae05c0d83
carol-profile <carolgitonga93@gmail.com>
2022-05-11 19:59:03 +0300

    Create config file

---
+++ config.py
@@ -0,0 +1,43 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://carol:lorac1234@localhost/pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    @staticmethod
+    def init_app(app):
+        pass
+    
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://carol:lorac1234@localhost/pitch'
+
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+
+}
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MAIL_USERNAME='carolgitonga93@gmail.com'
+export MAIL_PASSWORD='lorac@15'
+export SECRET_KEY='secret'
+python3 manage.py server
