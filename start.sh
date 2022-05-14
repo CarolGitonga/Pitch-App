4bc92e0a483339e8812da27c66f112f2ca09fde0
carol-profile <carolgitonga93@gmail.com>
2022-05-14 16:39:20 +0300

    deployment

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ export MAIL_USERNAME='carolgitonga93@gmail.com'
 export MAIL_PASSWORD='lorac@15'
 export SECRET_KEY='secret'
 export SQLALCHEMY_DATABASE_URI='postgresql://postgres:lorac1234@localhost/pitch'
-
+export DATABASE_URL=' postgres://cdghuxqtpybsmy:a3a755906462996c9d4063c005fb60aad6705ff3dfaee052f0e3db01f99498fb@ec2-3-224-164-189.compute-1.amazonaws.com:5432/d613lhack2bgj3'
 python3 manage.py server
